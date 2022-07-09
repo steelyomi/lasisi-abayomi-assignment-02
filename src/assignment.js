e0443860df34fff29ee5dfdc092f7ca6791b6f23
Lasisi Abayomi <47580735+steelyomi@users.noreply.github.com>
2022-07-09 00:22:28 +0000

    Attempted Challenges 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,12 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+ let arraySum = arrayOfNumbers.reduce((accumulator, a)=> accumulator + a, 0);
+  
+    return arraySum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +31,18 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+  let sumEvenArray = 0;
+  
+for(i=0; i<arrayOfNumbers.length; i++){
+  if(arrayOfNumbers[i]%2 === 0){
+    sumEvenArray++;
+  }
+} 
+  
+    return sumEvenArray;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
